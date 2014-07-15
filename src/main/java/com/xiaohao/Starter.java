0a9259f5797f9bd08a72dbfda0177b8cdf4b589d
superhao <xiaohaoixiaoli@126.com>
2014-07-15 10:28:36 +0800

    modify

--- src/main/java/com/xiaohao/Starter.java
+++ src/main/java/com/xiaohao/Starter.java
@@ -1,9 +1,25 @@
 package com.xiaohao;
 
+import com.xiaohao.config.AppConfig;
+import com.xiaohao.eg.Example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 /**
  * Created by xiaohao on 2014/7/14
  * app 启动器
  */
 public class Starter {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //拿到spring容器
+        ApplicationContext ac = new FileSystemXmlApplicationContext("./src/main/resources/applicationContext.xml");
+        //ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        Example e = (Example) ac.getBean("example");
+        e.test();
+    }
 
 }
--- src/main/java/com/xiaohao/lisenner/MyMessageDelegateListener.java
+++ src/main/java/com/xiaohao/lisenner/MyMessageDelegateListener.java
@@ -5,9 +5,15 @@ import org.springframework.data.redis.connection.MessageListener;
 
 /**
  * Created by xiaohao on 2014/7/14.
+ * 实现messageListenner接口 接收到消息后 处理   onMessage
  */
 public class MyMessageDelegateListener implements MessageListener {
 
+    /**
+     * 接收到消息 触发
+     * @param message
+     * @param bytes
+     */
     @Override
     public void onMessage(Message message, byte[] bytes) {
         System.out.println(new String(message.getBody()));
